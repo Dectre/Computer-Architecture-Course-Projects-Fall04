1cec6cbc34bd304a95fa4a01d64f89446a1323f7
Dectre <Amirali.dh84@gmail.com>
2025-12-05 02:50:32 +0330

    Complete CA2

--- CA2/Assembly/sort.s
+++ CA2/Assembly/sort.s
@@ -4,24 +4,24 @@
     addi x4, x0, 19
 LOOP1:
     slt x12, x2, x4
-    beq x12, x0, done
+    beq x12, x0, DONE
     addi x3, x0, 0
     addi x7, x1, 0
     sub  x9, x4, x2
 LOOP2:
     slt x10, x3, x9
-    beq x10, x0, outer_next
+    beq x10, x0, END_LOOP2
     lw  x5, 0(x7)
     lw  x6, 4(x7)
     slt x11, x6, x5
-    beq x11, x0, no_swap
+    beq x11, x0, NO_SWAP
     sw  x6, 0(x7)
     sw  x5, 4(x7)
-no_swap:
+NO_SWAP:
     addi x3, x3, 1
     addi x7, x7, 4
     jal  x0, LOOP2
-outer_next:
+END_LOOP2:
     addi x2, x2, 1
     jal  x0, LOOP1
-done:
+DONE:
