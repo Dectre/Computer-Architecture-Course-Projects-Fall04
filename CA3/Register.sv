aa8e3c8d926a56c00198c611b184d79a69568fc1
Dectre <Amirali.dh84@gmail.com>
2026-01-01 17:58:30 +0330

    Add register

--- CA3/Register.sv
+++ CA3/Register.sv
@@ -1,11 +1,11 @@
-module Register_12(d, load, clk, rst, q);
-    input [11:0] d;
+module Register #(parameter N = 16)(d, load, clk, rst, q);
+    input [N-1:0] d;
     input load, clk, rst;
-    output logic [11:0] q;
+    output logic [N-1:0] q;
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            q <= 12'b0;
+            q <= N'b0;
         end else if (load) begin
             q <= d;
         end
