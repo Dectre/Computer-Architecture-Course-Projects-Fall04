fa6651200510b7f6b53a201d122bec230213f1f1
Dectre <Amirali.dh84@gmail.com>
2026-01-01 18:31:11 +0330

    Edit DataMemory

--- CA3/DataMemory.sv
+++ CA3/DataMemory.sv
@@ -4,7 +4,7 @@ module DataMemory(A, WD, WE, clk, rst, RD);
     input WE, clk, rst;
     output logic [15:0] RD;
 
-    logic [35:0] memory [8191:0];
+    logic [15:0] memory [8191:0];
     always @(posedge clk or posedge rst) begin
         if (rst)
             $readmemh("data.mem", memory);
@@ -13,4 +13,3 @@ module DataMemory(A, WD, WE, clk, rst, RD);
 	end
     assign RD = memory[A];
 endmodule
-    
