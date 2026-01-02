03bdbd216efacdd1b5e9d580750993c3d39d977e
Dectre <Amirali.dh84@gmail.com>
2026-01-03 01:41:59 +0330

    Change Mux

--- CA4/src/Mux.sv
+++ CA4/src/Mux.sv
@@ -1,7 +1,7 @@
-module Mux_4to1_32(A, B, C, D, Sel, Y);
-    input [31:0] A, B, C, D;
+module Mux_4to1 #(parameter N  = 16)(A, B, C, D, Sel, Y);
+    input [N-1:0] A, B, C, D;
     input [1:0] Sel;
-    output logic [31:0] Y;
+    output logic [N-1:0] Y;
 
     always @(Sel, A, B, C, D) begin
         case (Sel)
@@ -9,21 +9,21 @@ module Mux_4to1_32(A, B, C, D, Sel, Y);
             2'b01: Y = B;
             2'b10: Y = C;
             2'b11: Y = D;
-            default: Y = 32'b0;
+            default: Y = {N{1'b0}};
         endcase
     end
 endmodule
 
-module Mux_2to1_32(A, B, Sel, Y);
-    input [31:0] A, B;
+module Mux_2to1 #(parameter N  = 16)(A, B, Sel, Y);
+    input [N-1:0] A, B;
     input Sel;
-    output logic [31:0] Y;
+    output logic [N-1:0] Y;
 
     always @(Sel, A, B) begin
         case (Sel)
             1'b0: Y = A;
             1'b1: Y = B;
-            default: Y = 32'b0;
+            default: Y = {N{1'b0}};
         endcase
     end
 endmodule
