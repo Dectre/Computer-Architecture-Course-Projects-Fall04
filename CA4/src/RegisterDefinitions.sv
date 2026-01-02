347fd56ec2dd54391486ba0e4e8cc0c7c10e0fed
Dectre <Amirali.dh84@gmail.com>
2026-01-03 01:42:17 +0330

    Fix some bugs in Package

--- CA4/src/RegisterDefinitions.sv
+++ CA4/src/RegisterDefinitions.sv
@@ -4,14 +4,15 @@ package PipeDefinitions;
         logic [31:0] PC;
         logic [31:0] Instr;
         logic [31:0] PCPlus4;
-    } IF_ID_t;
+    } IF_ID;
 
     typedef struct packed {
+        logic JumpSel;
         logic RegWrite;
         logic [1:0] ResultSrc;
         logic MemWrite;
         logic Jump;
-        logic Branch;
+        logic [1:0] Branch;
         logic [2:0] ALUControl;
         logic ALUSrc;
         logic [31:0] RD1;
@@ -22,7 +23,7 @@ package PipeDefinitions;
         logic [4:0] Rd;
         logic [31:0] ExtImm;
         logic [31:0] PCPlus4;
-    } ID_EX_t;
+    } ID_EX;
 
     typedef struct packed {
         logic RegWrite;
@@ -30,17 +31,19 @@ package PipeDefinitions;
         logic MemWrite;
         logic [31:0] ALUResult;
         logic [31:0] WriteData;
+        logic [31:0] PC;
         logic [4:0] Rd;
         logic [31:0] PCPlus4;
-    } EX_MEM_t;
+    } EX_MEM;
 
     typedef struct packed {
         logic RegWrite;
         logic [1:0] ResultSrc;
         logic [31:0] ALUResult;
         logic [31:0] ReadData;
+        logic [31:0] PC;
         logic [4:0] Rd;
         logic [31:0] PCPlus4;
-    } MEM_WB_t;
+    } MEM_WB;
 
 endpackage
